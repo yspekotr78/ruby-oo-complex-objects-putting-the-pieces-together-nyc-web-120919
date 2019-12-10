b479128a3640d6a7f5b0fa68e4dbd0b6b6863f42
Yury Spektor <yspektor78@gmail.com>
2019-12-10 20:21:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
-  attr_reader :title
+  
   
   def initialize(brand)
     @brand=brand
@@ -12,10 +12,7 @@ class Shoe
     @condition="new"
   end
   
-  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+
 
 end
 
