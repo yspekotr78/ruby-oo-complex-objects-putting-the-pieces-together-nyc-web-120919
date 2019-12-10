4e9305ab51744b504b3b53d49a2da2f5610eb5e1
Yury Spektor <yspektor78@gmail.com>
2019-12-10 20:19:45 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader :title
+  
+  def initialize(brand)
+    @brand=brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition="new"
+  end
+  
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
